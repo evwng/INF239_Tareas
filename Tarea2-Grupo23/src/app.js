e9c67cfdf850ce2e4bff3ffb6a8368c1c925e683
evwng <eva29wang@gmail.com>
2023-05-08 10:59:07 -0400

    08-05-2023

---
+++ Tarea2-Grupo23/src/app.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import UsersController from './controllers/UsersController.js';
+import morgan from 'morgan';
+
+const ENV = process.env;
+const app = express();
+
+//middleware
+app.use(express.json());
+app.use(morgan('dev'));
+
+//endpoints(Routes)
+app.get('/users', UsersController.getUsers)
+app.get('/users/:id', UsersController.getUserById)
+app.post('/users', UsersController.createUser)
+app.get('/users/:id/posts', UsersController.usersPosts)
+
+
+//==========================================================//
+app.get('/', (req, res) => {
+    res.json({ message: 'Hello World!!' });
+})
+//==========================================================//
+
+
+// 404 not found route
+app.use((_, res) => {
+    res.status(404).json({ message: 'Not found Crack!' });
+})
+
+
+//Init server
+app.listen(ENV.API_PORT, () => {
+    console.log(`Server running on port ${ENV.API_PORT}`);
+})
